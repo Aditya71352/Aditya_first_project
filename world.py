4b0ed6146b614771bffc560656d1cd90c0656d53
Aditya71352 <76588173+Aditya71352@users.noreply.github.com>
2022-03-07 14:21:41 +0530

    Add files via upload

---
+++ world.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask,render_template,request
+world=Flask(__name__)
+@world.route('/')
+def nitin():
+    return render_template('deployment.html')
+@world.route('/deployment',methods=['POST'])
+def abc():
+    if(request.method=="POST"):
+        n=request.form['num1']
+        m=request.form['num2']
+        total=int(n)+int(m)
+        
+        return render_template('deployment.html',aditya=total)
+if(__name__)=='__main__':
+    world.run()
+
+
+# In[4]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
